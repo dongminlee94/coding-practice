9b64dfe93bab61d6a19e1dfa222c1d3212b18252
dongminlee94 <kid33629@gmail.com>
2021-08-12 01:38:58 +0900

    Update TC and SC

--- src/1947_Maximum Compatibility Score Sum.py
+++ src/1947_Maximum Compatibility Score Sum.py
@@ -5,7 +5,7 @@ class Solution:
     def maxCompatibilitySum(self, students: List[List[int]], mentors: List[List[int]]) -> int:
         """
         M: len(students) / N: len(students[0])
-        TC: O(M! * N^2) / SC: O(M * N)
+        TC: O(M! * N^2) / SC: O(M + M!)
         Runtime: 2628 ms, faster than 33.54%
         Memory Usage: 14.2 MB, less than 95.44%
         """
@@ -20,7 +20,7 @@ class Solution:
             for m_idx in range(len(students)):
                 if m_idx not in assigned:  # TC: O(N)
                     score = sum([s == m for s, m in zip(students[s_idx], mentors[m_idx])])
-                    # dfs - SC: O(M) / assigned - SC: O(N)
+                    # dfs - SC: O(M) / assigned - SC: O(M!)
                     dfs(s_idx + 1, assigned + [m_idx], scores + score)
             return self.ans
 
