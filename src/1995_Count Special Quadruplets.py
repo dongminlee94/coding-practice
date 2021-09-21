10be4e5196af1a2386ca0793acfd3b951e5d692d
dongminlee94 <kid33629@gmail.com>
2021-09-22 04:31:34 +0900

    Add 1995 and 1996

---
+++ src/1995_Count Special Quadruplets.py
@@ -0,0 +1,44 @@
+"""
+1995. Count Special Quadruplets
+https://leetcode.com/problems/count-special-quadruplets/
+
+Example 1:
+
+Input: nums = [1,2,3,6]
+Output: 1
+Explanation: The only quadruplet that satisfies the requirement is (0, 1, 2, 3) because 1 + 2 + 3 == 6.
+
+Example 2:
+
+Input: nums = [3,3,6,4,5]
+Output: 0
+Explanation: There are no such quadruplets in [3,3,6,4,5].
+
+Example 3:
+
+Input: nums = [1,1,1,3,5]
+Output: 4
+Explanation: The 4 quadruplets that satisfy the requirement are:
+- (0, 1, 2, 3): 1 + 1 + 1 == 3
+- (0, 1, 3, 4): 1 + 1 + 3 == 5
+- (0, 2, 3, 4): 1 + 1 + 3 == 5
+- (1, 2, 3, 4): 1 + 1 + 3 == 5
+"""
+import itertools
+from typing import List
+
+
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        """
+        n: len(nums)
+        TC: O(n_C_4) / SC: O(2 * 4) <- (index + value) * 4
+        """
+        ans = 0
+        for quad in itertools.combinations(enumerate(nums), 4):
+            if (
+                quad[0][0] < quad[1][0] < quad[2][0] < quad[3][0]
+                and quad[0][1] + quad[1][1] + quad[2][1] == quad[3][1]
+            ):
+                ans += 1
+        return ans
---
+++ src/1996_The Number of Weak Characters in the Game.py
@@ -0,0 +1,67 @@
+"""
+1996. The Number of Weak Characters in the Game
+https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+
+Example 1:
+
+Input: properties = [[5,5],[6,3],[3,6]]
+Output: 0
+Explanation: No character has strictly greater attack and defense than the other.
+After sorting: [[6,3],[5,5],[3,6]]
+
+Example 2:
+
+Input: properties = [[2,2],[3,3]]
+Output: 1
+Explanation: The first character is weak because the second character has a strictly greater attack and defense.
+After sorting: [[3,3],[2,2]]
+
+Example 3:
+
+Input: properties = [[1,5],[10,4],[4,3]]
+Output: 1
+Explanation: The third character is weak because the second character has a strictly greater attack and defense.
+After sorting: [[10,4],[4,3],[1,5]]
+
+Example 4:
+
+Input: properties = [[2,1],[2,2],[1,1],[1,2]]
+Output: 1
+Explanation: The third character is weak because the second character has a strictly greater attack and defense.
+After sorting: [[2,1],[2,2],[1,1],[1,2]]
+
+Reference:
+https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/discuss/1445198/Python-Sort
+"""
+
+from typing import List
+
+
+class Solution:
+    def numberOfWeakCharacters1(self, properties: List[List[int]]) -> int:
+        """
+        Time Limit Exceeded
+        """
+        ans = 0
+        for i in range(len(properties)):
+            for j in range(len(properties)):
+                if properties[i][0] < properties[j][0] and properties[i][1] < properties[j][1]:
+                    ans += 1
+                    break
+        return ans
+
+    def numberOfWeakCharacters2(self, properties: List[List[int]]) -> int:
+        """
+        N: len(properties)
+        TC: O(NlogN) / SC: O(1)
+        """
+        ans = 0
+        properties.sort(key=lambda x: (-x[0], x[1]))
+
+        max_value = 0
+        for prop in properties:
+            if prop[1] < max_value:
+                ans += 1
+            else:
+                max_value = prop[1]
+        return ans
