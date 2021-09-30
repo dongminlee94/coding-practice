a29a3fc3eed90b1ae055bc74f3483eda4d276be3
dongminlee94 <kid33629@gmail.com>
2021-09-30 20:59:44 +0900

    Add 2011

---
+++ src/2011_Final Value of Variable After Performing Operations.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+2011. Final Value of Variable After Performing Operations
+https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+
+Example 2:
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+
+Example 3:
+
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+"""
+from typing import List
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        """
+        N: len(operations)
+        TC: O(N) / SC: O(1)
+        """
+        ans = 0
+        for oper in operations:
+            if oper == "++X" or oper == "X++":
+                ans += 1
+            else:
+                ans -= 1
+        return ans
