a6ea19f08b89d3e46d99d20e1c8097276c7bd07d
dongminlee94 <kid33629@gmail.com>
2022-01-31 14:46:23 +0900

    Add array problems

---
+++ src/top-interview-questions-easy/array/1_remove-duplicates-from-sorted-array.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+727. Remove Duplicates from Sorted Array
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+"""
+
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        TC: O(N) / SC: O(1)
+        """
+        idx = 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] != nums[i]:
+                nums[idx] = nums[i]
+                idx += 1
+        return idx
---
+++ src/top-interview-questions-easy/array/2_best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+564. Best Time to Buy and Sell Stock II
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        TC: O(N) / SC: O(1)
+        """
+        start = end = prices[0]
+        ans = 0
+        for i in range(1, len(prices)):
+            if end >= prices[i]:
+                if end - start > 0:
+                    ans += end - start
+                start = end = prices[i]
+            else:
+                end = prices[i]
+        ans += end - start
+        return ans
