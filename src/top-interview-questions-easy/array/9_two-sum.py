e35819a43689b85e381917da3925ba3514aa10cb
dongminlee94 <kid33629@gmail.com>
2022-02-09 22:39:09 +0900

    Add array problem 9

---
+++ src/top-interview-questions-easy/array/9_two-sum.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+9. Two Sum
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/546/
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        TC: O(N^2) / SC: O(1)
+        """
+        for i in range(len(nums)):
+            value, nums[i] = nums[i], -10
+            if target - value in nums:
+                return [i, nums.index(target - value)]  # O(N)
+
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        """
+        1) Put (target - value) in advance
+        2) and then check if there's (target - value) in sum_dict
+
+        TC: O(N) / SC: O(N)
+        """
+        sum_dict = {}
+        for i, value in enumerate(nums):
+            if value in sum_dict:
+                return [sum_dict[value], i]
+            sum_dict[target - value] = i
