3f504a6e593c01ad14fbad284f394a4e14815f5a
dongminlee94 <kid33629@gmail.com>
2022-03-18 16:56:52 +0900

    Add string problems 6

---
+++ src/top-interview-questions-easy/strings/06_string-to-integer-atoi.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+6. String to Integer (atoi) *
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+"""
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        sign = False
+        ans = ""
+        for c in s:
+            if c == "-" or c == "+":
+                if not sign:
+                    ans += c
+                    sign = True
+                else:
+                    break
+            elif c.isdigit():
+                ans += c
+                sign = True
+            else:
+                break
+
+        if not ans or (len(ans) == 1 and not ans[0].isdigit()):
+            return 0
+        else:
+            ans = int(ans)
+            if ans > 2 ** 31 - 1:
+                return 2 ** 31 - 1
+            elif ans < -(2 ** 31):
+                return -(2 ** 31)
+            else:
+                return ans
