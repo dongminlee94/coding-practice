3fdc9ff128bff5b1888505f940b9eb5f92c508a8
dongminlee94 <kid33629@gmail.com>
2022-04-17 22:29:24 +0900

    Update pre-commit and github action

--- src/weekly_contest_258/2000_reverse-prefix-of-word.py
+++ src/weekly_contest_258/2000_reverse-prefix-of-word.py
@@ -8,7 +8,7 @@ Example 1:
 
 Input: word = "abcdefd", ch = "d"
 Output: "dcbaefd"
-Explanation: The first occurrence of "d" is at index 3. 
+Explanation: The first occurrence of "d" is at index 3.
 Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
 
 Example 2:
