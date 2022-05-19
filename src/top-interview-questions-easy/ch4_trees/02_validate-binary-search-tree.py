806f8b5615c1d1271188cdbe114beffd02253e28
dongminlee94 <kid33629@gmail.com>
2022-05-20 00:21:44 +0900

    Add tree problem 2

---
+++ src/top-interview-questions-easy/ch4_trees/02_validate-binary-search-tree.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+2. Validate Binary Search Tree *
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/625/
+"""
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        N: The number of nodes
+        TC: O(N) / SC: O(N)
+        """
+        stack = []
+        trav = root
+        prev = -float("inf")
+        while trav or stack:
+            if trav:
+                stack.append(trav)
+                trav = trav.left
+            else:
+                node = stack.pop()
+                if node:
+                    if node.val <= prev:
+                        return False
+                    prev = node.val
+                trav = node.right
+        return True
